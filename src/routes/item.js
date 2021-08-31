fe2a209e3fdc74e4b2369222a29457bf50544271
enmanuelCastillo5 <enmanuel.castillo@hotmail.com>
2021-08-31 18:01:31 -0400

    termiando Crud route

--- src/routes/item.js
+++ src/routes/item.js
@@ -1,10 +1,53 @@
 const express = require('express')
 const router = express.Router()
+const Item = require('../models/Item')
 
+//get Items
 router.get('/', (req, res) => {
-    res.json({
-        success: true
+    Item.find((error, items) => {
+        if (error) {throw error }
+        else {
+            res.json(items)
+        }
     })
 });
+
+router.post('/', (req, res) => {
+    const item = new Item(req.body)
+    item.save().then(item => {
+        res.status(200).json({item: 'item agregado'})
+    })
+    .catch(error => {
+        res.status(400).send({item: 'error al agregar item'})
+    })
+})
+
+router.put('/:id', (req, res, next) => {
+    Item.findById(req.params.id, (error, item) => {
+        if (!item) {
+            return next(new Error('no se pudo encontrar el documento'));
+        } else {
+            item.name = req.body.name;
+            item.price = req.body.price
+            item.save()
+            .then(item => {
+                res.json('item actualizado')
+            })
+            .catch(error => {
+                res.status(400).send('no se pudo actualizar')
+            })
+        }
+
+    })
+});
+
+router.delete('/:id', (req, res, next) => {
+    Item.findByIdAndDelete(req.params.id, (error, item) => {
+        if (error) { res.json(error) }
+        else {
+            res.json('item eliminado')
+        }
+    })
+})
 
 module.exports = router;
